bd34f9c4be488bcd9cb9d92407d1d9a404863aa1
steph-spencer <stephanie.e.spencer@gmail.com>
2020-04-30 23:20:18 -0400

    initial commit
    Added project files and created array of quotes

---
+++ js/script.js
@@ -0,0 +1,97 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+      quote: "I've always been more interested in the future than in the past",
+      source: "Grace Hopper",
+      citation: "Reader's Digest",
+      year: 1984
+  },
+  {
+      quote: "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution. It is, strictly speaking, a real factor in scientific research.",
+      source: "Albert Einstein",
+      citation:"Cosmic Religion : With Other Opinions and Aphorisms",
+      year: 1931
+  },
+  {
+      quote: "Science, my lad, has been built upon many errors; but they are errors which it was good to fall into, for they led to the truth",
+      source: "Jules Verne",
+      citation: "Journey to the Center of the Earth",
+      year: 1864
+  },
+  {
+      quote: "What is a scientist after all? It is a curious person looking through a keyhole, the keyhole of nature, trying to know what's going on",
+      source: "Jacques-Yves Cousteau",
+      citation: "Christian Science Monitor",
+      year: 1971
+  },
+  {
+      quote: "The passion and beauty and joy of science is that we humans have invented a process to understand the universe in a way that is true for everyone. We are finding universal truths.",
+      source: "Bill Nye",
+      citation: "The Star-Ledger",
+      year: 2000
+  },
+  {
+      quote: "All adventures, especially into new territory, are scary.",
+      source: "Sally Ride",
+  },
+  {
+      quote: "Facts do not cease to exist because they are ignored.",
+      source: "Aldous Huxley",
+  },
+  {
+      quote: "I am always doing what I cannot do yet, in order to learn how to do it.",
+      source: "Vincent Van Gogh"
+  },
+  {
+      quote: "In any given moment we have two options: to step forward into growth, or step back into safety.",
+      source: "Abraham Maslow"
+  },
+  {
+      quote: "Forever is composed of nows.",
+      source: "Emily Dickinson"
+  },
+  {
+      quote: "When you're a little different than others, it takes a little more time to fit in. If you really want to be there, there are no ingredients you need other than effort, perseverance and teamwork.",
+      source: "Julie Payette",
+      citation: "Macleans",
+      year: 2015
+  },
+  {
+      quote: "Tact is the art of making a point without making an enemy",
+      source: "Sir Isaac Newton"
+  },
+  {
+      quote: "Don't ask yourself what the world needs. Ask yourself what makes you come alive and then go do that. Because what the world needs is people who have come alive",
+      source: "Howard Thurman",
+  }
+];
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
